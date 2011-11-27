1da8b28212fb7bd9155c580ee8f3c6f99ed998f8
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-11-27 10:35:00 +0100

    add /commits/:branch view

--- liberator.rb
+++ liberator.rb
@@ -33,11 +33,38 @@ helpers do
     src = "https://secure.gravatar.com/avatar/#{hash}?s=#{size}&amp;d=#{missing}"
     "<img class=\"gravatar\" height=\"#{size}\" width=\"#{size}\" src=\"#{src}\">"
   end
+
+  def group_by_committed_date(commits)
+    cs = commits.group_by { |c| c.committed_date.strftime('%Y-%m-%d') }.sort
+    cs.reverse!
+    cs.map! { |d,cs| [Time.parse(d), cs] }
+  end
+
+  def branch_or_404(branch)
+    branch = branch.to_s
+    branches = repo.branches.map(&:name)
+    raise NotFound unless branches.include?(branch)
+    branch
+  end
 end
 
 get '/' do
   @commits = repo.commits
   erb :index
+end
+
+get '/commits' do
+  redirect '/commits/master'
+end
+
+get '/commits/:branch' do
+  commits_limit = 75
+  branch = branch_or_404(params[:branch])
+  commits = repo.commits(branch, commits_limit)
+  @group_commits = group_by_committed_date(commits).map do |d, cs|
+    [d.strftime('%b %d, %Y'), cs]
+  end
+  erb :commits
 end
 
 get '/commit/:sha' do
